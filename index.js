266840cf216adacb91e68ea608780aa7e1dd9cf1
JenniferKiesler <111549041+JenniferKiesler@users.noreply.github.com>
2022-10-24 21:46:11 -0500

    adds code

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-console.log('test')
+// first npm init
+// then create file .gitignore
+
+import inquirer from "inquirer";
+import chalkAnimation from 'chalk-animation'
+import {generateSlug} from 'random-word-slugs'
+
+inquirer.prompt([
+    {
+        type: "number",
+        name: "length",
+        message: "How long should the band name be?",
+        default: 3,
+    },
+    {
+        type: 'list',
+        name: "animationName",
+        message: "Which animation would you like to use?",
+        default: "rainbow",
+        choices: ["rainbow", "pulse", "glitch", "radar", "neon", "karaoke"]
+    }
+])
+    .then((answers) => {
+        console.log(answers)
+        
+        const bandName = generateSlug(answers.length, {format: 'title'})
+        
+        chalkAnimation[answers.animationName](bandName)
+    })
+    .catch(err => console.log(err))
+
+
